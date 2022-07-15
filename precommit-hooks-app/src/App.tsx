590f731f6b303e9cda452a7b4bd35a16e0d03bcb
sherry naeem <sherrynaeem2@hotmail.com>
2022-07-15 11:28:02 +1000

    testing

--- precommit-hooks-app/src/App.tsx
+++ precommit-hooks-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <p>Adding another line of code</p>
+          <p>Adding another line of code..</p>
         </p>
         <a
           className="App-link"
